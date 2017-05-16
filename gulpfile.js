d90c9db2fc35a36079e92044f33cd2e602df163c
Kevin <kevin@pixelkojan.se>
2017-05-16 15:19:19 +0200

    configure gulp to rename and minify CSS. Adjust folders in public to hold source (development) and dist (production)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
-    return gulp.src('./public/scss/*.scss')
+    return gulp.src('./public/source/scss/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/dist/css'));
+});
+
+// minify CSS with clean-css and put in dist folder
+gulp.task('minifyCSS', function() {
+    return gulp.src('./public/source/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('./public/dist/css'));
 });
 
 gulp.task('default', function() {
