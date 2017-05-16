a29fb5fd09c3cbd53e307e01dd55a199921cfc27
Kevin <kevin@pixelkojan.se>
2017-05-16 14:35:25 +0200

    remove car.sort.js and edit in test file

--- test/front-end-test.js
+++ test/front-end-test.js
@@ -1,7 +1,5 @@
 var chai = require('chai');
 var expect = chai.expect;
-var carSort = require('../public/js/car.sort').carSort;
-var bmw = require('../public/js/car.sort').bmw;
 
 // This is a test suite, a block of unit tests that belong together
 // First argument describe what we test, second just wrapper callback
@@ -12,13 +10,14 @@ describe('Mocha', function () {
     });
 });
 
-// Test carSort
-describe('Sort a Car depending on properties', function () {
-    it('Log out a car', function () {
-        expect(carSort()).to.be.a('string');
-        expect(bmw).to.be.a('string');
-    });
-    it('Is bmw a string?', function () {
-        expect(bmw.model).to.equal('bmw');
-    });
-});
+// Old test, saved as example
+// // Test carSort
+// describe('Sort a Car depending on properties', function () {
+//     it('Log out a car', function () {
+//         expect(carSort()).to.be.a('string');
+//         expect(bmw).to.be.a('string');
+//     });
+//     it('Is bmw a string?', function () {
+//         expect(bmw.model).to.equal('bmw');
+//     });
+// });
